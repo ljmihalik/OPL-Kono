ac0a73fc2f9b7efffb99f8db1702ef67fc71f159
ljmihalik <lauren.j.mihalik@gmail.com>
2018-01-31 11:30:01 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -8,6 +8,21 @@ class Game{
         //if winner is brought in then dont need to throw dice
         void startGame(string winner); 
 
+        void saveGame(int roundNum, Board& table, Human& person, Computer& AI);
+
+        void loadGame(Board& table);
+        
+        bool humanTurn;
+    
+    private:
+    /*
+        int round;
+        int Cscore;
+        int Ccolor;
+        int Hscore;
+        int Hcolor;
+        string nextplayer;
+        */
        
     
 
